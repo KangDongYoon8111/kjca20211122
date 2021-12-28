0e0ce0e7d58ae472e0d7549e215970e38208b86e
koo0642 <96402773+koo0642@users.noreply.github.com>
2021-12-28 11:50:59 +0900

    Add files via upload

---
+++ Chapter3/ChoiYEBon20211228.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiYEBon20211228 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 30;
+        int b = 40;
+        int c = a * b;
+            
+        Debug.Log($"사각형의 너비는{a}입니다");
+        Debug.Log($"사각형의 높이는{b} 입니다");
+        Debug.Log($"사각형의넓이는:{c}") ;
+
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
