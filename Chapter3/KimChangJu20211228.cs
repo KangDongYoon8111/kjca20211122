a316c38937f8fe155c6810c85585184e2c83ed30
honeypolarbear <96402765+honeypolarbear@users.noreply.github.com>
2021-12-28 12:03:47 +0900

    Add files via upload

---
+++ Chapter3/KimChangJu20211228.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimChangJu20211228: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string a = " 30 ";
+        string b = " 40 ";
+
+        int c  = int.Parse(a);
+        int d  = int.Parse(b);
+
+
+        Debug.Log("사각형의 너비는" + a + "입니다");
+        Debug.Log("사각형의 높이는" + b + "입니다");
+        Debug.Log("사각형의 넓이는" + c*d +"입니다");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
