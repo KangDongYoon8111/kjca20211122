b4df1a0688f23ff5694d58f9b89b1228fabe3334
강동윤 <46252404+KangDongYoon8111@users.noreply.github.com>
2021-12-28 10:33:55 +0900

    Create KangDongYoon20211228.cs

---
+++ Chapter3/KangDongYoon20211228.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KangDongYoon20211228 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string a = "30";
+        string b = "40";
+
+        // 이 곳에 사각형의 넓이를 계산하고
+        // 출력하는 코드를 추가하세요.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
