54c5977a737c95cefe46db7d273d093e957b0729
kanggeunu <96159116+kanggeunu@users.noreply.github.com>
2021-12-30 11:59:16 +0900

    Update and rename kanageunu20211230 to kanageunu20211230.cs

--- Chapter4/kanageunu20211230.cs
+++ Chapter4/kanageunu20211230.cs
@@ -6,8 +6,8 @@ i++와 ++i의 차이점은 i++는 먼저 결과값을 더하고, ++i는
 int i = 1;
 Debug.Log(i = i + 1);	답 : 2
 Debug.Log(i++);		답 :1
-Debug.Log(++i);		답 :1
-Debug.Log(i += 1);		답 :1
+Debug.Log(++i);		답 :2
+Debug.Log(i += 1);		답 :2
 
 3. 다음 코드에서 b는 어떤 값을 가질까요? 
 int a = 10;
