eff117fbb8c27e883274403cfb3c8bc125554fb0
강동윤 <46252404+KangDongYoon8111@users.noreply.github.com>
2021-12-31 10:46:02 +0900

    Create KangDongYoon_EX2.cs

---
+++ Chapter5/KangDongYoon_EX2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KangDongYoon_EX2 : MonoBehaviour
+{
+    void Start()
+    {
+        // 정수 한 개를 입력받아서,
+        // 그 수가 50 이상의 수인지
+        // 50미만의 수인지 판단하는 프로그램을 만들어 주세요.
+    }
+
+    void Update()
+    {
+
+    }
+}
