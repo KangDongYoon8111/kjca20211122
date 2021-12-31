0ebdd530c1512b0686dfa6a705b578447c844b5e
강동윤 <46252404+KangDongYoon8111@users.noreply.github.com>
2021-12-31 10:42:14 +0900

    Create KangDongYoon20211231_EX1.cs

---
+++ Chapter5/KangDongYoon20211231_EX1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter5 : MonoBehaviour
+{
+    void Start()
+    {
+        // 1~10까지의 정수 중에서 3으로 나누어 떨어지지 않는 수를 출력하고, 이 수들의 합을 출력
+    }
+
+    void Update()
+    {
+
+    }
+}
