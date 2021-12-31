dd63c00d6d4523b8a4985685dad1e04babac0bd7
minsu12345 <90177639+minsu12345@users.noreply.github.com>
2021-12-31 12:15:44 +0900

    Add files via upload

---
+++ Chapter5/KimMinSu_EX1.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimMinSu_EX1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 69;
+        if(a >= 90)
+        {
+            Debug.Log("A");
+           
+        }
+        else
+        {
+            if (a >= 80)
+            {
+                Debug.Log("B");
+            
+            }
+            else
+            {
+                if (a >= 70)
+                {
+                    Debug.Log("C");
+                }
+                else
+                {
+                    Debug.Log("F");
+                }
+            }
+        }
+        }
+        
+    }
+
+
+    // 
