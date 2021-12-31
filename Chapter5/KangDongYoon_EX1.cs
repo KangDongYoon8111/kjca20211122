36ea49fc035d86d02296229141db9bf9f29590b6
강동윤 <46252404+KangDongYoon8111@users.noreply.github.com>
2021-12-31 10:44:41 +0900

    Update and rename KangDongYoon20211231_EX1.cs to KangDongYoon_EX1.cs

---
+++ Chapter5/KangDongYoon_EX1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter5 : MonoBehaviour
+{
+    void Start()
+    {
+        // 학점 안내 프로그램
+        // 사용자의 점수를 입력받아 90점 이상으 "A",
+        // 80점 이상은 "B", 70점 이상은 "C",
+        // 69점 이하 점수는 "F"가 출력되는 프로그램을
+        // 만들어 주세요.
+    }
+
+    void Update()
+    {
+
+    }
+}
