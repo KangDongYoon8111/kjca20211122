8c019f7324d7dab7ac56eff91e649ad05cb961b4
MinuChoi <96402772+CODEMANMINU@users.noreply.github.com>
2021-12-31 12:25:08 +0900

    Add files via upload

---
+++ Chapter5/ChoiMinWoo_EX1.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter5 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()        
+    {
+        string userInput = "110";
+
+        int number = int.Parse(userInput);
+        if (number >= 90)
+        {
+            Debug.Log("A");
+        }
+        else if (number >= 80)
+        {
+            Debug.Log("B");
+        }
+        else if (number >= 70)
+        {
+            Debug.Log("C");
+        }
+        else
+        {
+            Debug.Log("F");
+        }
+    
+    
+
+    }
+    private void Update()
+    {
+
+
+    }
+}
+  
+        
+
+
