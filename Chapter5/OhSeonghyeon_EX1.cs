4851973f5f4dbd83545de009b1f3410ff82a4c5f
O-S-H <96241313+O-S-H@users.noreply.github.com>
2021-12-31 12:35:31 +0900

    Update OhSeonghyeon_EX1.cs

--- Chapter5/OhSeonghyeon_EX1.cs
+++ Chapter5/OhSeonghyeon_EX1.cs
@@ -7,19 +7,19 @@ public class OhSeonghyeon_EX1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string userInput = "70";
+       string userInput = "70";
         int number = int.Parse(userInput);
-        if(number<=100)
+        if (number <= 100)
         {
-            if(number >= 90)
+            if (number >= 90)
             { Debug.Log("A"); }
             else if (number >= 80)
             { Debug.Log("B"); }
             else if (number >= 70)
             { Debug.Log("C"); }
-        }
-        else if (number <= 69)
-        { Debug.Log("F"); }
+            else if (number <= 69)
+            { Debug.Log("F"); }
+        }  
 
 
 
