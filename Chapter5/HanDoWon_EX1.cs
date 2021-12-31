6de41c19229ee846cb8d474c4a4d579dbdba774b
dimpledowon <96159073+dimpledowon@users.noreply.github.com>
2021-12-31 12:11:57 +0900

    Add files via upload

---
+++ Chapter5/HanDoWon_EX1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HanDoWon_EX1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string userinput = "50";
+
+        int number = int.Parse(userinput);
+        if (number >= 70)
+        {
+            if(number>90)
+            {
+                Debug.Log("A");
+            }
+
+            else if (number > 80)
+            {
+                Debug.Log("B");
+            }
+
+            else
+            {
+                Debug.Log("C");
+            }
+        }
+        else
+        {
+            Debug.Log("D");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
