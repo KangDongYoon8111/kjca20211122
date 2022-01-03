b4822ea2dcac554c44583bf12e20daa4908ae7c9
ohsangmok <96159092+ohsangmok@users.noreply.github.com>
2022-01-03 14:40:00 +0900

    Add files via upload

---
+++ Chapter5/OhSangMokEX1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OhSangMokEX1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 74;
+        if (a >= 90)
+        { Debug.Log("a"); }
+        else if (a >= 80)
+        { Debug.Log("b"); }
+        else if (a >= 70)
+        { Debug.Log("c"); }
+        else 
+        { Debug.Log("f"); }
+    
+
+    }
+
+
+
+
+
+
+
+// Update is called once per frame
+   void Update()
+    { }
+
+
+
+
+
+
+}
+    
+        
+    
