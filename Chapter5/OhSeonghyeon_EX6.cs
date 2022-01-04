37c9d961a635a3b1dd625097ae9060c71f0f38f8
O-S-H <96241313+O-S-H@users.noreply.github.com>
2022-01-04 11:18:49 +0900

    Update OhSeonghyeon_EX6.cs

--- Chapter5/OhSeonghyeon_EX6.cs
+++ Chapter5/OhSeonghyeon_EX6.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chapter5_6 : MonoBehaviour
+public class OhSeonghyeon_EX6: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
