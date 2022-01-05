f61236bbde3f9b6c9b9564eea66d4c8d275c7410
niadia <96159100+niadia@users.noreply.github.com>
2022-01-05 18:33:03 +0900

    Add files via upload

---
+++ Chapter5/LeeJiHye2022010504.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeeJiHye2022010504 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start is called before the first frame update
+        //사용자로부터 세 개의 정수를 입력받아, 
+        //그 수들의 최대값과 최소값,
+        //합계와 평균을 구하는 프로그램을
+        //만들어주세요.
+
+        string Userinput1 = "100";
+        string Userinput2 = "70";
+        string Userinput3 = "10";
+
+        int max = 0;
+        int min = 0;
+        int sum = 0;
+        float average = 0.0f;
+
+        
+        {
+            int a = int.Parse(Userinput1);
+            int b = int.Parse(Userinput2);
+            int c = int.Parse(Userinput3);
+
+            Max(a, b, c);
+            Min(a, b, c);
+            Sum(a, b, c);
+            Average(a, b, c);
+        }
+
+        void Max(int a, int b, int c)
+        {
+            max = a;
+
+            if (a >= max)
+            {
+                max = a;
+            }
+            if (b >= max)
+            {
+                max = b;
+            }
+            if (c >= max)
+            {
+                max = c;
+            }
+            Debug.Log($"큰수: {max}");
+        }
+        void Min(int a, int b, int c)
+        {
+            min = a;
+
+            if (a <= min)
+            {
+                min = a;
+            }
+            if (b <= min)
+            {
+                min = b;
+            }
+            if (c <= min)
+            {
+                min = c;
+            }
+            Debug.Log($"작은수: {min}");
+        }
+        void Sum(int a, int b, int c)
+        {
+            sum = a + b + c;
+            Debug.Log($"합계: {sum}");
+        }
+
+        void Average(int a, int b, int c)
+        {
+            average = (a + b + c) / 3;
+            Debug.Log($"평균: {average}");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
