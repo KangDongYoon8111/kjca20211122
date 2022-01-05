4a310c9ca57091b78c0447a41cccfdb58cb86480
beck <90177717+beck0369@users.noreply.github.com>
2022-01-05 11:04:47 +0900

    Add files via upload

---
+++ Chapter5/BeakDaYoung_EX3.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeakDaYoung_EX3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 10;
+
+        Debug.Log(a % 3 == 0 ? "3의 배수가 맞습니다." : "3의 배수가 아닙니다.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
