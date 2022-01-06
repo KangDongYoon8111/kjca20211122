fff284ef89878ed950e974e7a9e4dcf94e8ccf26
dimpledowon <96159073+dimpledowon@users.noreply.github.com>
2022-01-06 11:18:20 +0900

    Update and rename HanDoWon_Ex6.cs to HanDoWon_EX6.cs

--- Chapter5/HanDoWon_EX6.cs
+++ Chapter5/HanDoWon_EX6.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HanDoWon_Ex6 : MonoBehaviour
+public class HanDoWon_EX6 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
