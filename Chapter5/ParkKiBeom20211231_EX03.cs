e6c138f0324da320a8064d0ccc8738c77aec5b04
AlienaDorothy <72484058+AlienaDorothy@users.noreply.github.com>
2022-01-09 14:30:46 +0900

    Update ParkKiBeom20211231_EX03.cs

--- Chapter5/ParkKiBeom20211231_EX03.cs
+++ Chapter5/ParkKiBeom20211231_EX03.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParkKiBeom20211231_EX3 : MonoBehaviour
+public class ParkKiBeom20211231_EX03 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
