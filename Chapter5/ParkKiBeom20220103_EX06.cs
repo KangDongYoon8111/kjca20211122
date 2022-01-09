e6e4cedc2a8df7b6e8f6040cb027c128be420123
AlienaDorothy <72484058+AlienaDorothy@users.noreply.github.com>
2022-01-09 14:32:30 +0900

    Update and rename ParkKiBeom20220103_EX6.cs to ParkKiBeom20220103_EX06.cs

--- Chapter5/ParkKiBeom20220103_EX06.cs
+++ Chapter5/ParkKiBeom20220103_EX06.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParkKiBeom20220103_EX6 : MonoBehaviour
+public class ParkKiBeom20220103_EX06 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
