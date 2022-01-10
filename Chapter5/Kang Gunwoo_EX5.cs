4af1f52a5500f6362e00917a8aa37980d0954f19
KangGunWoo <96159116+kanggeunu@users.noreply.github.com>
2022-01-10 11:38:42 +0900

    Rename KanaGeonU_EX5.cs to Kang Gunwoo_EX5.cs

--- Chapter5/Kang Gunwoo_EX5.cs
+++ Chapter5/Kang Gunwoo_EX5.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class KanaGeonU20211231_EX5 : MonoBehaviour
 {
-//    �����ʵ��б����� 1�г���� 4�г���� �߰�����
-//������ ���Ҵ�.
-//4�г��� 70�� �̻��̸� �հ�, �� �̿��� �г��� 60��
-//�̻��̸� �հ��̴�.
-//�̸� �Ǵ��ϴ� ���α׷��� ������ּ���.
-//������ 0�̸� 100�ʰ��̸� ������� ���
+//    광주초등학교에서 1학년부터 4학년까지 중간고사
+//시험을 보았다.
+//4학년은 70점 이상이면 합격, 그 이외의 학년은 60점
+//이상이면 합격이다.
+//이를 판단하는 프로그램을 만들어주세요.
+//점수가 0미만 100초과이면 경고문구 출력
     // Start is called before the first frame update
     void Start()
     {
@@ -18,32 +18,32 @@ public class KanaGeonU20211231_EX5 : MonoBehaviour
 
         if( score < 0 || score > 100)
         {
-            Debug.Log("�߸��� score�Դϴ�");
+            Debug.Log("잘못된 score입니다");
         }
         else if(grade==4)
         {
             if (score >= 70)
             {
-                Debug.Log("�հ�");
+                Debug.Log("합격");
             }
             else
             {
-                Debug.Log("���հ�");
+                Debug.Log("불합격");
             }
         }else if(grade<4 && grade >= 1)
         {
             if (score >= 60)
             {
-                Debug.Log("�հ�");
+                Debug.Log("합격");
             }
             else
             {
-                Debug.Log("���հ�");
+                Debug.Log("불합격");
             }
         }
         else
         {
-            Debug.Log("�߸��� �г��� �Է��ϼ̽��ϴ�");
+            Debug.Log("잘못된 학년을 입력하셨습니다");
         }
 
         
