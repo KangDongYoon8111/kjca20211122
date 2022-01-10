a7928abcc65f2b163307c155256173a66dab7bbd
yj3677 <90177601+yj3677@users.noreply.github.com>
2022-01-10 13:34:27 +0900

    Update KimYoungJoo.EX1.cs

--- Chapter6/KimYoungJoo.EX1.cs
+++ Chapter6/KimYoungJoo.EX1.cs
@@ -1,12 +1,21 @@
- public string userInput1 = "3";
+  public string userInput1 = "3";
+    public string userInput2 = "34.2";
     void Start()
     {
-        Debug.Log(Square());
+        Debug.Log("결과:"+userInput1+"의 제곱값은"+Square()+"9입니다.");
+        Debug.Log("결과:"+userInput2+"의 제곱값은"+Square()+"9입니다.");
     }
 
     int Square()
     {
-        int number = int.Parse(userInput1);
+        int number = int.Parse(userInput1);    
         number *= number;
         return number;
+  
+    }
+    int Square2()
+    {
+        int number2 = int.Parse(userInput2);
+        number2 *= number2;
+        return number2;
     }
