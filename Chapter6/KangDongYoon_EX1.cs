89ff817fb3b5e3e56884869113d508c3507a1275
강동윤 <46252404+KangDongYoon8111@users.noreply.github.com>
2022-01-10 12:35:00 +0900

    Update KangDongYoon_EX1.cs

--- Chapter6/KangDongYoon_EX1.cs
+++ Chapter6/KangDongYoon_EX1.cs
@@ -1,1 +1,14 @@
+using UnityEngine;
 
+public class KangDongYoon_EX1 : MonoBehaviour
+{
+    void Start()
+    {
+        string userInput1 = "3";
+        string userInput2 = "34.2";
+        //...
+        //...
+    }
+
+    // 제곱값을 구하는 메소드를 구현해 주세요.
+}
